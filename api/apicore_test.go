782e114e474908931a0bf3ed4a38fa49fc691d53
Jeff Palm <jeff@jeffpalm.com>
2022-05-12 08:25:57 -0400

    convert to stupid go tests

--- api/apicore_test.go
+++ api/apicore_test.go
@@ -20,91 +20,147 @@ func makeClient(t *testing.T) *core {
 }
 
 func TestGetLegislators(t *testing.T) {
-	runTest := func(id string) {
-		client := makeClient(t)
-		info, err := client.GetLegislators(id)
-		if err != nil {
-			t.Fatalf("GetLegislators(%q): %v", id, err)
-		}
-		found := false
-		const firstlast = "Nancy Pelosi"
-		for _, li := range info.LegislatorInfos {
-			if li.Firstlast == firstlast {
-				found = true
-				break
-			}
-		}
-		if !found {
-			t.Fatalf("GetLegislators(%q): did not find expecting firstlast %s", id, firstlast)
-		}
+	tests := []struct {
+		name string
+		id   string
+	}{
+		{
+			name: "state",
+			id:   "CA",
+		},
+		{
+			name: "person",
+			id:   "N00007360",
+		},
 	}
-
-	runTest("CA")
-	runTest("N00007360")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			id := tt.id
+			client := makeClient(t)
+			info, err := client.GetLegislators(id)
+			if err != nil {
+				t.Fatalf("GetLegislators(%q): %v", id, err)
+			}
+			found := false
+			const firstlast = "Nancy Pelosi"
+			for _, li := range info.LegislatorInfos {
+				if li.Firstlast == firstlast {
+					found = true
+					break
+				}
+			}
+			if !found {
+				t.Fatalf("GetLegislators(%q): did not find expecting firstlast %s", id, firstlast)
+			}
+		})
+	}
 }
 
 func TestGetMemPFDprofile(t *testing.T) {
-	runTest := func(cid string, year int) {
-		client := makeClient(t)
-		info, err := client.GetMemPFDprofile(cid, GetMemPFDprofileYear(year))
-		if err != nil {
-			t.Fatalf("GetMemPFDprofile(%q,%d): %v", cid, year, err)
-		}
-		if want, got := "Pelosi, Nancy", info.Name; want != got {
-			t.Errorf("GetMemPFDprofile(%q,%d).Name: got %q, wanted %q", cid, year, got, want)
-		}
-		if year != 0 {
-			if want, got := fmt.Sprintf("%d", year), info.DataYear; want != got {
-				t.Errorf("GetMemPFDprofile(%q,%d).DataYear: got %q, wanted %q", cid, year, got, want)
+	tests := []struct {
+		name string
+		cid  string
+		year int
+	}{
+		{
+			name: "no year",
+			cid:  "N00007360",
+		},
+		{
+			name: "with year",
+			cid:  "N00007360",
+			year: 2016,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cid, year := tt.cid, tt.year
+			client := makeClient(t)
+			info, err := client.GetMemPFDprofile(cid, GetMemPFDprofileYear(year))
+			if err != nil {
+				t.Fatalf("GetMemPFDprofile(%q,%d): %v", cid, year, err)
+			}
+			if want, got := "Pelosi, Nancy", info.Name; want != got {
+				t.Errorf("GetMemPFDprofile(%q,%d).Name: got %q, wanted %q", cid, year, got, want)
+			}
+			if year != 0 {
+				if want, got := fmt.Sprintf("%d", year), info.DataYear; want != got {
+					t.Errorf("GetMemPFDprofile(%q,%d).DataYear: got %q, wanted %q", cid, year, got, want)
+				}
 			}
 
-		}
+		})
 	}
-
-	runTest("N00007360", 0)
-	runTest("N00007360", 2016)
 }
 
 func TestGetCandSummary(t *testing.T) {
-	runTest := func(cid string, cycle int) {
-		client := makeClient(t)
-		info, err := client.GetCandSummary(cid, GetCandSummaryCycle(cycle))
-		if err != nil {
-			t.Fatalf("GetCandSummary(%q,%d): %v", cid, cycle, err)
-		}
-		if want, got := "Pelosi, Nancy", info.CandSummaryInfo.CandName; want != got {
-			t.Errorf("GetCandSummary(%q,%d).CandName: got %q, wanted %q", cid, cycle, got, want)
-		}
-		if cycle != 0 {
-			if want, got := fmt.Sprintf("%d", cycle), info.CandSummaryInfo.Cycle; want != got {
-				t.Errorf("GetCandSummary(%q,%d).Cycle: got %q, wanted %q", cid, cycle, got, want)
-			}
-
-		}
+	tests := []struct {
+		name  string
+		cid   string
+		cycle int
+	}{
+		{
+			name: "no cycle",
+			cid:  "N00007360",
+		},
+		{
+			name:  "with cycle",
+			cid:   "N00007360",
+			cycle: 2016,
+		},
 	}
-
-	runTest("N00007360", 0)
-	runTest("N00007360", 2016)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cid, cycle := tt.cid, tt.cycle
+			client := makeClient(t)
+			info, err := client.GetCandSummary(cid, GetCandSummaryCycle(cycle))
+			if err != nil {
+				t.Fatalf("GetCandSummary(%q,%d): %v", cid, cycle, err)
+			}
+			if want, got := "Pelosi, Nancy", info.CandSummaryInfo.CandName; want != got {
+				t.Errorf("GetCandSummary(%q,%d).CandName: got %q, wanted %q", cid, cycle, got, want)
+			}
+			if cycle != 0 {
+				if want, got := fmt.Sprintf("%d", cycle), info.CandSummaryInfo.Cycle; want != got {
+					t.Errorf("GetCandSummary(%q,%d).Cycle: got %q, wanted %q", cid, cycle, got, want)
+				}
+			}
+		})
+	}
 }
 
 func TestGetCandContrib(t *testing.T) {
-	runTest := func(cid string, cycle int) {
-		client := makeClient(t)
-		info, err := client.GetCandContrib(cid, GetCandContribCycle(cycle))
-		if err != nil {
-			t.Fatalf("GetCandContrib(%q,%d): %v", cid, cycle, err)
-		}
-		if want, got := "Nancy Pelosi (D)", info.Recipient.CandName; want != got {
-			t.Errorf("GetCandContrib(%q,%d).CandName: got %q, wanted %q", cid, cycle, got, want)
-		}
-		if cycle != 0 {
-			if want, got := fmt.Sprintf("%d", cycle), info.Recipient.Cycle; want != got {
-				t.Errorf("GetCandContrib(%q,%d).Cycle: got %q, wanted %q", cid, cycle, got, want)
-			}
-
-		}
+	tests := []struct {
+		name  string
+		cid   string
+		cycle int
+	}{
+		{
+			name: "no cycle",
+			cid:  "N00007360",
+		},
+		{
+			name:  "with cycle",
+			cid:   "N00007360",
+			cycle: 2016,
+		},
 	}
-
-	runTest("N00007360", 0)
-	runTest("N00007360", 2016)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cid, cycle := tt.cid, tt.cycle
+			client := makeClient(t)
+			info, err := client.GetCandContrib(cid, GetCandContribCycle(cycle))
+			if err != nil {
+				t.Fatalf("GetCandContrib(%q,%d): %v", cid, cycle, err)
+			}
+			if want, got := "Nancy Pelosi (D)", info.Recipient.CandName; want != got {
+				t.Errorf("GetCandContrib(%q,%d).CandName: got %q, wanted %q", cid, cycle, got, want)
+			}
+			if cycle != 0 {
+				if want, got := fmt.Sprintf("%d", cycle), info.Recipient.Cycle; want != got {
+					t.Errorf("GetCandContrib(%q,%d).Cycle: got %q, wanted %q", cid, cycle, got, want)
+				}
+			}
+		})
+	}
 }
