cd11cfbe53a46b28e4073ff06e11fbc929570165
Jeff Palm <jeff@jeffpalm.com>
2022-05-12 08:18:16 -0400

    more tests

--- api/apicore_test.go
+++ api/apicore_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/spudtrooper/goutil/io"
@@ -26,7 +27,7 @@ func TestGetLegislators(t *testing.T) {
 			t.Fatalf("GetLegislators(%q): %v", id, err)
 		}
 		found := false
-		const firstlast = "John Kennedy"
+		const firstlast = "Nancy Pelosi"
 		for _, li := range info.LegislatorInfos {
 			if li.Firstlast == firstlast {
 				found = true
@@ -38,6 +39,72 @@ func TestGetLegislators(t *testing.T) {
 		}
 	}
 
-	runTest("LA")
-	runTest("N00026823")
+	runTest("CA")
+	runTest("N00007360")
+}
+
+func TestGetMemPFDprofile(t *testing.T) {
+	runTest := func(cid string, year int) {
+		client := makeClient(t)
+		info, err := client.GetMemPFDprofile(cid, GetMemPFDprofileYear(year))
+		if err != nil {
+			t.Fatalf("GetMemPFDprofile(%q,%d): %v", cid, year, err)
+		}
+		if want, got := "Pelosi, Nancy", info.Name; want != got {
+			t.Errorf("GetMemPFDprofile(%q,%d).Name: got %q, wanted %q", cid, year, got, want)
+		}
+		if year != 0 {
+			if want, got := fmt.Sprintf("%d", year), info.DataYear; want != got {
+				t.Errorf("GetMemPFDprofile(%q,%d).DataYear: got %q, wanted %q", cid, year, got, want)
+			}
+
+		}
+	}
+
+	runTest("N00007360", 0)
+	runTest("N00007360", 2016)
+}
+
+func TestGetCandSummary(t *testing.T) {
+	runTest := func(cid string, cycle int) {
+		client := makeClient(t)
+		info, err := client.GetCandSummary(cid, GetCandSummaryCycle(cycle))
+		if err != nil {
+			t.Fatalf("GetCandSummary(%q,%d): %v", cid, cycle, err)
+		}
+		if want, got := "Pelosi, Nancy", info.CandSummaryInfo.CandName; want != got {
+			t.Errorf("GetCandSummary(%q,%d).CandName: got %q, wanted %q", cid, cycle, got, want)
+		}
+		if cycle != 0 {
+			if want, got := fmt.Sprintf("%d", cycle), info.CandSummaryInfo.Cycle; want != got {
+				t.Errorf("GetCandSummary(%q,%d).Cycle: got %q, wanted %q", cid, cycle, got, want)
+			}
+
+		}
+	}
+
+	runTest("N00007360", 0)
+	runTest("N00007360", 2016)
+}
+
+func TestGetCandContrib(t *testing.T) {
+	runTest := func(cid string, cycle int) {
+		client := makeClient(t)
+		info, err := client.GetCandContrib(cid, GetCandContribCycle(cycle))
+		if err != nil {
+			t.Fatalf("GetCandContrib(%q,%d): %v", cid, cycle, err)
+		}
+		if want, got := "Nancy Pelosi (D)", info.Recipient.CandName; want != got {
+			t.Errorf("GetCandContrib(%q,%d).CandName: got %q, wanted %q", cid, cycle, got, want)
+		}
+		if cycle != 0 {
+			if want, got := fmt.Sprintf("%d", cycle), info.Recipient.Cycle; want != got {
+				t.Errorf("GetCandContrib(%q,%d).Cycle: got %q, wanted %q", cid, cycle, got, want)
+			}
+
+		}
+	}
+
+	runTest("N00007360", 0)
+	runTest("N00007360", 2016)
 }
--- scripts/integtest.sh
+++ scripts/integtest.sh
@@ -8,20 +8,20 @@ scripts=$(dirname $0)
 
 # TODO: Add these as unit tests, also.
 $scripts/run.sh
-$scripts/run.sh GetLegislators --id LA
-$scripts/run.sh GetLegislators --id N00026823
-$scripts/run.sh GetMemPFDprofile --cid N00026823
-$scripts/run.sh GetMemPFDprofile --cid N00026823 --year 2016
-$scripts/run.sh GetCandSummary --cid N00026823
-$scripts/run.sh GetCandSummary --cid N00026823 --cycle 2016
-$scripts/run.sh GetCandContrib --cid N00026823
-$scripts/run.sh GetCandContrib --cid N00026823 --cycle 2016
-$scripts/run.sh GetCandIndustry --cid N00026823
-$scripts/run.sh GetCandIndustry --cid N00026823 --cycle 2016
-$scripts/run.sh GetCandByInd --cid N00026823 --ind M02
-$scripts/run.sh GetCandByInd --cid N00026823 --ind M02 --cycle 2016
-$scripts/run.sh GetCandSector --cid N00026823
-$scripts/run.sh GetCandSector --cid N00026823 --cycle 2016
+$scripts/run.sh GetLegislators --id CA
+$scripts/run.sh GetLegislators --id N00007360
+$scripts/run.sh GetMemPFDprofile --cid N00007360
+$scripts/run.sh GetMemPFDprofile --cid N00007360 --year 2016
+$scripts/run.sh GetCandSummary --cid N00007360
+$scripts/run.sh GetCandSummary --cid N00007360 --cycle 2016
+$scripts/run.sh GetCandContrib --cid N00007360
+$scripts/run.sh GetCandContrib --cid N00007360 --cycle 2016
+$scripts/run.sh GetCandIndustry --cid N00007360
+$scripts/run.sh GetCandIndustry --cid N00007360 --cycle 2016
+$scripts/run.sh GetCandByInd --cid N00007360 --ind M02
+$scripts/run.sh GetCandByInd --cid N00007360 --ind M02 --cycle 2016
+$scripts/run.sh GetCandSector --cid N00007360
+$scripts/run.sh GetCandSector --cid N00007360 --cycle 2016
 $scripts/run.sh GetCongCmteIndus -cmte HARM -ind F10
 $scripts/run.sh GetCongCmteIndus -cmte HARM -ind F10  --congno 11
 $scripts/run.sh GetOrgs --org Goldman
